3cb207d4b37900fc083e04978f5788a316ffd662
AaloCat <101341618+AaloCat@users.noreply.github.com>
2022-06-03 11:28:16 +0200

    huge progress socket io wery gud

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,39 @@
-function rendertable() {
-    var table = document.createElement("table");
-    var thead = document.createElement("thead");
+function rendertable(data) {
+  var table = document.createElement("table");
+  var thead = document.createElement("thead");
+  // Betrag, Verwendungszweck, Datum
+  var amounttd = document.createElement("th");
+  amounttd.innerText = "Amount";
+  thead.appendChild(amounttd);
+
+  var usageth = document.createElement("th");
+  usageth.innerText = "Usage";
+  thead.appendChild(usageth);
+
+  var dateth = document.createElement("th");
+  dateth.innerText = "Date";
+  thead.appendChild(dateth);
+
+  var tbody = document.createElement("tbody");
+  //Display every transaction in its own tr
+  data.forEach((transaction) => {
+    var tr = document.createElement("tr");
+
+    var amounttd = document.createElement("td");
+    amounttd.innerHTML = transaction.amount;
+    tr.appendChild(amounttd);
+
+    var usagetd = document.createElement("td");
+    usagetd.innerHTML = transaction.usage;
+    tr.appendChild(usagetd);
+
+    var datetd = document.createElement("td");
+    datetd.innerHTML = transaction.date;
+    tr.appendChild(datetd);
+  });
 }
+
+const socket = io();
+socket.on("connect", () =>
+  socket.emit("hello", `Hi there! I am ${window.navigator.userAgent}`)
+);
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-let express = require("express"); // handles requests + responses
-let path = require("path"); // path module provides utilities for working with file and directory paths
-let bodyParser = require("body-parser"); //needed to parse request params
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// create app
-let app = express();
-
-// configure app
-// Serve Static Assets
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-//configure port
-const PORT = process.env.PORT || 8080;
-
-// Serve Static Assets
 app.use(express.static("public"));
 
-app.listen(PORT, () => {
-  console.log("Server connected at:", PORT);
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+
+io.on("connection", (socket) => {
+  console.log(socket.handshake.address + " connected");
+
+  socket.on("disconnect", function () {
+    console.log(socket.handshake.address + " disconnected!");
+  });
+
+  io.on("hello", (hellomsg) => {
+    console.log("hellomessage sents");
+  });
+});
+
+server.listen(8080, () => {
+  console.log("listening on: 8080");
 });
