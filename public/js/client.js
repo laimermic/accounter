220be922007c4243a0565c2bf92afad9c4c1564c
AaloCat <101341618+AaloCat@users.noreply.github.com>
2022-06-03 13:09:17 +0200

    hate css

--- public/js/client.js
+++ public/js/client.js
@@ -1,35 +1,36 @@
-function rendertable(data) {
-  var table = document.createElement("table");
-  var thead = document.createElement("thead");
+function renderTable(data) {
+  let table = document.createElement("table");
+  let thead = document.createElement("thead");
   // Betrag, Verwendungszweck, Datum
-  var amounttd = document.createElement("th");
+  let amounttd = document.createElement("th");
   amounttd.innerText = "Amount";
   thead.appendChild(amounttd);
 
-  var usageth = document.createElement("th");
+  let usageth = document.createElement("th");
   usageth.innerText = "Usage";
   thead.appendChild(usageth);
 
-  var dateth = document.createElement("th");
+  let dateth = document.createElement("th");
   dateth.innerText = "Date";
   thead.appendChild(dateth);
 
-  var tbody = document.createElement("tbody");
+  let tbody = document.createElement("tbody");
   //Display every transaction in its own tr
   data.forEach((transaction) => {
-    var tr = document.createElement("tr");
+    let tr = document.createElement("tr");
 
-    var amounttd = document.createElement("td");
+    let amounttd = document.createElement("td");
     amounttd.innerHTML = transaction.amount;
     tr.appendChild(amounttd);
 
-    var usagetd = document.createElement("td");
+    let usagetd = document.createElement("td");
     usagetd.innerHTML = transaction.usage;
     tr.appendChild(usagetd);
 
-    var datetd = document.createElement("td");
+    let datetd = document.createElement("td");
     datetd.innerHTML = transaction.date;
     tr.appendChild(datetd);
+    tbody.appendChild(tr);
   });
 }
 
@@ -37,3 +38,10 @@ const socket = io();
 socket.on("connect", () =>
   socket.emit("hello", `Hi there! I am ${window.navigator.userAgent}`)
 );
+
+let testTable = [
+  { amount: 48, usage: "Essen", date: "18.1.2022" },
+  { amount: 28, usage: "Hund", date: "19.2.2022" },
+];
+
+renderTable(testTable);
