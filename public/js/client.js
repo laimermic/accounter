fb57eee421a97c4f3d93a90174529cc8203fd9e6
AaloCat <101341618+AaloCat@users.noreply.github.com>
2022-06-03 14:02:02 +0200

    MongoDB + MORE CSS

--- public/js/client.js
+++ public/js/client.js
@@ -1,22 +1,28 @@
-function renderTable(data) {
+function renderIncomeTable(data) {
   let table = document.createElement("table");
   let thead = document.createElement("thead");
-  // Betrag, Verwendungszweck, Datum
-  let amounttd = document.createElement("th");
-  amounttd.innerText = "Amount";
-  thead.appendChild(amounttd);
+  let tbody = document.createElement("tbody");
+
+  //Header for date, usage and amount
+  let headtr = document.createElement("tr");
+
+  let amountth = document.createElement("th");
+  amountth.innerText = "Amount";
+  headtr.appendChild(amountth);
 
   let usageth = document.createElement("th");
   usageth.innerText = "Usage";
-  thead.appendChild(usageth);
+  headtr.appendChild(usageth);
 
   let dateth = document.createElement("th");
   dateth.innerText = "Date";
-  thead.appendChild(dateth);
+  headtr.appendChild(dateth);
 
-  let tbody = document.createElement("tbody");
+  thead.appendChild(headtr);
+
+  table.appendChild(thead);
   //Display every transaction in its own tr
-  data.forEach((transaction) => {
+  data.income.forEach((transaction) => {
     let tr = document.createElement("tr");
 
     let amounttd = document.createElement("td");
@@ -32,16 +38,82 @@ function renderTable(data) {
     tr.appendChild(datetd);
     tbody.appendChild(tr);
   });
+  table.appendChild(tbody);
+  document.getElementsByClassName("accountIncome")[0].appendChild(table);
+}
+
+function renderExpensesTable(data) {
+  let table = document.createElement("table");
+  let thead = document.createElement("thead");
+  let tbody = document.createElement("tbody");
+
+  //Header for date, usage and amount
+  let dateth = document.createElement("th");
+  dateth.innerText = "Date";
+  thead.appendChild(dateth);
+
+  let usageth = document.createElement("th");
+  usageth.innerText = "Usage";
+  thead.appendChild(usageth);
+
+  let amounttd = document.createElement("th");
+  amounttd.innerText = "Amount";
+  thead.appendChild(amounttd);
+
+  //Display every transaction in its own tr
+  data.expenses.forEach((transaction) => {
+    let tr = document.createElement("tr");
+
+    let amounttd = document.createElement("td");
+    amounttd.innerHTML = transaction.amount;
+    tr.appendChild(amounttd);
+
+    let usagetd = document.createElement("td");
+    usagetd.innerHTML = transaction.usage;
+    tr.appendChild(usagetd);
+
+    let datetd = document.createElement("td");
+    datetd.innerHTML = transaction.date;
+    tr.appendChild(datetd);
+    tbody.appendChild(tr);
+  });
+  table.appendChild(tbody);
+  document.getElementsByClassName("accountExpenses")[0].appendChild(table);
+}
+
+function rendertotal(data) {
+  let total = 0;
+  data.income.forEach((transaction) => {
+    total += transaction.amount;
+  });
+  data.expenses.forEach((transaction) => {
+    total -= transaction.amount;
+  });
+
+  document.getElementsByClassName("accountTotalspan")[0].innerHTML =
+    "Total: " + total;
+  if (total < 0) {
+    document.getElementsByClassName("accountTotalspan")[0].style.color = "red";
+  } else if (total == 0) {
+    document.getElementsByClassName("accountTotalspan")[0].style.color =
+      "black";
+  } else {
+    document.getElementsByClassName("accountTotalspan")[0].style.color =
+      "green";
+  }
 }
 
 const socket = io();
-socket.on("connect", () =>
-  socket.emit("hello", `Hi there! I am ${window.navigator.userAgent}`)
-);
+socket.on("connect", () => {
+  socket.emit("queryData", "I hätt gern die Daten");
+  socket.on("");
+});
 
-let testTable = [
-  { amount: 48, usage: "Essen", date: "18.1.2022" },
-  { amount: 28, usage: "Hund", date: "19.2.2022" },
-];
+let testTable = {
+  income: [{ amount: 48, usage: "Food", date: "18.1.2022" }],
+  expenses: [{ amount: 28, usage: "Dog", date: "19.2.2022" }],
+};
 
-renderTable(testTable);
+renderIncomeTable(testTable);
+renderExpensesTable(testTable);
+rendertotal(testTable);
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const MongoClient = require("mongodb").MongoClient;
+const connectionUrl = "mongodb://mongoadmin:mypasswd@10.115.3.9:8017";
 app.use(express.static("public"));
 
 // app.get("/", (req, res) => {
@@ -12,14 +13,31 @@ app.use(express.static("public"));
 // });
 
 io.on("connection", (socket) => {
+  socket.client._packet;
   console.log(socket.handshake.address + " connected");
 
   socket.on("disconnect", function () {
     console.log(socket.handshake.address + " disconnected!");
   });
 
-  io.on("hello", (hellomsg) => {
-    console.log("hellomessage sents");
+  socket.on("queryData", () => {
+    console.log(
+      "Client " +
+        socket.handshake.address +
+        "requested Data...\n Connecting to MongoDB..."
+    );
+    MongoClient.connect(connectionUrl, (err, db) => {
+      if (err) throw err;
+      let dataBase = db.db("mydb");
+      dataBase
+        .collection("accounter")
+        .find({})
+        .toArray((err, result) => {
+          if (err) throw err;
+          console.log(result);
+          dataBase.close();
+        });
+    });
   });
 });
 
